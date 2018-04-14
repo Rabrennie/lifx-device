f24439f9a2583fef2e7388cdca831ba1a8cc0b20
Rabrennie <Rabrennie@users.noreply.github.com>
2018-04-14 17:18:17 +0100

    implement Lifx handle method

--- include/Lifx.h
+++ include/Lifx.h
@@ -5,6 +5,8 @@
 #include "LifxMessage.h"
 #include "LifxConstants.h"
 
+#include <WiFiUDP.h>
+
 class Lifx {
     public:
         Lifx(LifxDevice *device);
@@ -13,9 +15,11 @@ class Lifx {
         void handle();
     private:
         LifxDevice *device;
+        WiFiUDP Udp;
 
         void sendMessage(LifxMessage message, bool broadcast);
         void processPacket(byte *packet, int packetSize, LifxMessage &lifxMessage);
+        void handleMessage(LifxMessage lifxMessage);
 };
 
 #endif
--- src/Lifx.cpp
+++ src/Lifx.cpp
@@ -3,11 +3,22 @@
 Lifx::Lifx(LifxDevice *device) {
     this->device = device;
 }
-
+// TODO: add debug serial prints
 void Lifx::begin() {
-    // do listen stuff
+    this->Udp.begin(LIFX_PORT);
 }
 
 void Lifx::handle() {
     // do other stuff
+    byte packet[LIFX_PACKET_LENGTH];
+
+    int packetSize = this->Udp.parsePacket();
+    if(packetSize) {
+        this->Udp.read(packet, LIFX_PACKET_LENGTH);
+
+        LifxMessage message;
+        this->processPacket(packet, packetSize, message);
+
+        this->handleMessage(message);
+    }   
 }
