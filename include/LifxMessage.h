64acbccf46e1e9207be534efd346b37fc671b56a
Rabrennie <Rabrennie@users.noreply.github.com>
2018-04-14 18:25:05 +0100

    update LifxMessage to contain correct fields

--- include/LifxMessage.h
+++ include/LifxMessage.h
@@ -2,7 +2,27 @@
 #define LIFX_MESSAGE_HEADER
 
 struct LifxMessage {
-
+    /* frame */
+  uint16_t size;
+  uint16_t protocol:12;
+  uint8_t  addressable:1;
+  uint8_t  tagged:1;
+  uint8_t  origin:2;
+  uint32_t source;
+  /* frame address */
+  uint8_t  target[8];
+  uint8_t  reserved[6];
+  uint8_t  res_required:1;
+  uint8_t  ack_required:1;
+  uint8_t  :6;
+  uint8_t  sequence;
+  /* protocol header */
+  uint64_t :64;
+  uint16_t type;
+  uint16_t :16;
+  /* variable length payload follows */
+  byte payload[128];
+  int payload_size;
 };
 
 #endif
